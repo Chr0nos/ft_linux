ca70704eab7d27810e4fbbfd56e473e295e59127
Chr0nos <snicolet@student.42.fr>
2018-03-23 00:35:11 +0000

    fix freetype

--- xorg/ebuild/freetype.sh
+++ xorg/ebuild/freetype.sh
@@ -3,7 +3,7 @@ build() {
 	VERSION=2.9
 	URL=https://downloads.sourceforge.net/$PKG/$PKG-$VERSION.tar.bz2
 	pull $PKG bz2 $VERSION $URL
-	unpack $PKG-$VERSION
+	unpack $PKG-$VERSION bz2
 	sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg
 	sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
 		-i include/freetype/config/ftoption.h  &&
