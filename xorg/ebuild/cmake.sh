cca0524c57ff0fafb28384b239f4730f07c4266a
Chr0nos <snicolet@student.42.fr>
2018-03-23 18:54:50 +0000

    fixes on cmake and libuv

--- xorg/ebuild/cmake.sh
+++ xorg/ebuild/cmake.sh
@@ -5,7 +5,7 @@ build() {
 	VERSION="3.10.2"
 	URL="https://cmake.org/files/v3.10/$PKG-$VERSION.tar.gz"
 	pull $PKG gz $VERSION $URL
-	unpack $PKG-$VERSION
+	unpack $PKG-$VERSION gz
 	sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
 
 	./bootstrap --prefix=/usr        \
--- xorg/ebuild/libuv.sh
+++ xorg/ebuild/libuv.sh
@@ -3,7 +3,7 @@ build() {
 	VERSION="1.19.2"
 	URL="https://dist.libuv.org/dist/v$VERSION/$PKG-v$VERSION.tar.gz"
 	pull $PKG gz $VERSION $URL
-	unpack $PKG-$VERSION
+	unpack $PKG-v$VERSION gz $PKG-$VERSION.tar.gz
 	sh autogen.sh
 	./configure --prefix=/usr --disable-static
 	compile $PKG-$VERSION
