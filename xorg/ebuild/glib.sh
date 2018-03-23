8571d4fe50a52a0c5fa137e42e0d5b58db217dfe
Chr0nos <snicolet@student.42.fr>
2018-03-23 23:59:22 +0000

    added glib

---
+++ xorg/ebuild/glib.sh
@@ -0,0 +1,12 @@
+build() {
+	PKG="glib"
+	VERSION="2.56.0"
+	URL="http://ftp.gnome.org/pub/gnome/sources/$PKG/2.56/$PKG-$VERSION.tar.xz"
+	pull $PKG xz $VERSION $URL
+	unpack $PKG-$VERSION xz
+	./configure --prefix=/usr      \
+				--with-pcre=system \
+				--with-python=/usr/bin/python3 &&
+		compile $PKG-$VERSION
+	cleanup $PKG $VERSION
+}
