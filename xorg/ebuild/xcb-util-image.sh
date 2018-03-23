022fba2855efc1ec5955a1846f250c3b6873380a
Chr0nos <snicolet@student.42.fr>
2018-03-23 00:33:09 +0000

    fix unpack

--- xorg/ebuild/xcb-util-image.sh
+++ xorg/ebuild/xcb-util-image.sh
@@ -3,7 +3,7 @@ build() {
 	VERSION=0.4.0
 	URL=https://xcb.freedesktop.org/dist/$PKG-$VERSION.tar.bz2
 	pull $PKG bz2 $VERSION $URL
-	unpack $PKG-$VERSION
+	unpack $PKG-$VERSION bz2
 	./configure $XORG_CONFIG
 	compile $PKG-$VERSION
 	make install
