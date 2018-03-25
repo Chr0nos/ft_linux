85b785a1e37d2494fb7af1261b8be1dd75fc1460
Chr0nos <snicolet@student.42.fr>
2018-03-25 17:24:25 +0000

    fix

--- xorg/ebuild/fribidi.sh
+++ xorg/ebuild/fribidi.sh
@@ -3,6 +3,8 @@ build() {
 	URL="https://github.com/$PKG/$PKG/releases/download/v$VERSION/$PKG-$VERSION.tar.bz2"
 	pull $PKG bz2 $VERSION $URL
 	unpack $PKG-$VERSION bz2
-	meson --prefix=/usr && compile $PKG-$VERSION && make install
+	mkdir -v build
+	cd build
+	meson --prefix=/usr .. && compile $PKG-$VERSION && make install
 	cleanup
 }
