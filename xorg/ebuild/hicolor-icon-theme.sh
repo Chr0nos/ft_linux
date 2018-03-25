0c78bf35ded7b0240ea81499ebbaddd87f4a5101
Chr0nos <snicolet@student.42.fr>
2018-03-25 22:20:38 +0000

    typo

--- xorg/ebuild/hicolor-icon-theme.sh
+++ xorg/ebuild/hicolor-icon-theme.sh
@@ -1,6 +1,6 @@
 build() {
 	VERSION="0.17"
-	URL=" https://icon-theme.freedesktop.org/releases/$PKG-$VERSION.tar.xz"
+	URL="https://icon-theme.freedesktop.org/releases/$PKG-$VERSION.tar.xz"
 	prepair $PKG $VERSION xz $URL
 	./configure --prefix=/usr && compile $PKG-$VERSIOn && make install && cleanup
 }
