788fa79b730fd1fd52042f8af89512e03e6cf44a
Chr0nos <snicolet@student.42.fr>
2018-03-25 14:22:34 +0000

    shared-mime-info seems to build on 1 thread max

--- xorg/ebuild/shared-mime-info.sh
+++ xorg/ebuild/shared-mime-info.sh
@@ -3,6 +3,6 @@ build() {
 	URL="https://people.freedesktop.org/~hadess/shared-mime-info-$VERSION.tar.xz"
 	pull $PKG xz $VERSION $URL
 	unpack $PKG-$VERSION xz
-	./configure --prefix=/usr && compile $PKG-$VERSION && make install
+	./configure --prefix=/usr && compile $PKG-$VERSION 1 && make install
 	cleanup
 }
