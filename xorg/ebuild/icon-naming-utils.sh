2aee25bdd333d1dead357633678e0751d05e7516
Chr0nos <snicolet@student.42.fr>
2018-03-25 22:19:29 +0000

    typo

--- xorg/ebuild/icon-naming-utils.sh
+++ xorg/ebuild/icon-naming-utils.sh
@@ -1,6 +1,6 @@
 build() {
 	VERSION="0.8.90"
-	URL=" http://tango.freedesktop.org/releases/$PKG-$VERSION.tar.bz2"
+	URL="Shttp://tango.freedesktop.org/releases/$PKG-$VERSION.tar.bz2"
 	prepair $PKG $VERSION bz2 $URL
 	./configure --prefix=/usr && compile $PKG-$VERSION && make install && cleanup
 }
