d32dc51848c00c2ca430b4aae675e5fac3f1594b
Chr0nos <snicolet@student.42.fr>
2018-03-25 22:35:04 +0000

    typo

--- xorg/ebuild/at-spi2-atk.sh
+++ xorg/ebuild/at-spi2-atk.sh
@@ -1,6 +1,6 @@
 build() {
 	VERSION="2.26.1"
 	URL="http://ftp.gnome.org/pub/gnome/sources/$PKG/2.26/$PKG-$VERSION.tar.xz"
-	prepair $PKG $VERSIOn xz $URL
+	prepair $PKG $VERSION xz $URL
 	./configure --prefix=/usr && compile $PKG-$VERSION && make install && cleanup
 }
