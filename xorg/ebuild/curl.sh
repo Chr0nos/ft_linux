4b3c9e2044a68292110a3cb1f7931c96f27e4473
Chr0nos <snicolet@student.42.fr>
2018-03-25 22:06:34 +0000

    added curl

---
+++ xorg/ebuild/curl.sh
@@ -0,0 +1,18 @@
+build() {
+	VERSION="7.60.0-20180321"
+	URL="https://curl.haxx.se/snapshoots/curl-$VERSION.tar.xz"
+	prepair $PKG $VERSION xz $URL
+	./configure --prefix=/usr                           \
+				--disable-static                        \
+				--enable-threaded-resolver              \
+				--with-ca-path=/etc/ssl/certs &&
+		compile $PKG-$VERSION && make install
+	rm -rf docs/examples/.deps &&
+	find docs \( -name Makefile\* \
+				-o -name \*.1       \
+				-o -name \*.3 \)    \
+				-exec rm {} \;      &&
+
+	install -v -d -m755 /usr/share/doc/curl-$VERSION &&
+	cp -v -R docs/*     /usr/share/doc/curl-$VERSION
+}
