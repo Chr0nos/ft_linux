0dbbd75eea86f5e027112623e39bc8abf02cdeac
Chr0nos <snicolet@student.42.fr>
2018-03-25 21:50:38 +0000

    fix

--- xorg/ebuild/ca-certificates.sh
+++ xorg/ebuild/ca-certificates.sh
@@ -1,5 +1,8 @@
 build() {
+	mkdir -pv $SRCS/$PKG
+	cd $SRCS/$PKG
 	curl -k https://curl.haxx.se/ca/cacert.pem | \
 		awk '{print > "cert" (1+n) ".pem"} /-----END CERTIFICATE-----/ {n++}'
 	c_rehash
+	rm -rf $SRCS/$PKG
 }
