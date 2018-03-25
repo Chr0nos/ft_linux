c0205739a0b1a4b34f719fa6745812582cf43435
Chr0nos <snicolet@student.42.fr>
2018-03-25 22:26:17 +0000

    up gtk3

---
+++ xorg/ebuild/gtk3.sh
@@ -0,0 +1,15 @@
+build() {
+	VERSION="3.22.28"
+	URL="http://ftp.gnome.org/pub/gnome/sources/gtk+/3.22/gtk+-$VERSION.tar.xz"
+	pull $PKG xz $VERSION $URL
+	unpack gtk+-$VERSION xz $PKG-$VERSION.tar.xz
+	./configure --prefix=/usr             \
+				--sysconfdir=/etc         \
+				--enable-broadway-backend \
+				--enable-x11-backend      \
+				--enable-wayland-backend &&
+		compile $PKG-$VERSION && make install
+	gtk-query-immodules-3.0 --update-cache
+	glib-compile-schemas /usr/share/glib-2.0/schemas
+	cleanup
+}
