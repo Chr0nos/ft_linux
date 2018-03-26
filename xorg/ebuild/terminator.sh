23ebb8d975b356bfed7b2c8eac8faf0a764e2dab
Chr0nos <snicolet@student.42.fr>
2018-03-26 00:26:19 +0000

    fix

--- xorg/ebuild/terminator.sh
+++ xorg/ebuild/terminator.sh
@@ -2,5 +2,5 @@ build() {
 	VERSION="1.91"
 	URL="https://launchpad.net/terminator/gtk3/1.91/+download/terminator-1.91.tar.gz"
 	prepair $PKG $VERSION gz $URL
-	./configure --prefix=/usr && compile $PKG-$VERSION && make install && cleanup
+	python ./setup.py install && cleanup
 }
