03b0e148a703018f369a66d54e961986ea5da154
Chr0nos <snicolet@student.42.fr>
2018-04-08 19:30:52 +0000

    added pygobject

---
+++ xorg/ebuild/pygobject.sh
@@ -0,0 +1,27 @@
+build_python2() {
+    mkdir python2 &&
+        pushd python2 &&
+        ../configure --prefix=/usr --with-python=/usr/bin/python &&
+        compile $PKG &&
+        popd
+    cd ..
+}
+
+build_python3() {
+    mkdir python3 &&
+        pushd python3 &&
+        ../configure --prefix=/usr --with-python=/usr/bin/python3 &&
+        compile $PKG &&
+        popd
+    cd ..
+}
+
+build() {
+    VERSION="3.28.2"
+    EXT="xz"
+    URL="http://ftp.gnome.org/pub/gnome/sources/$PKG/3.28/$PKG-$VERSION.tar.$EXT"
+    prepair $PKG $VERSION $EXT $URL
+    build_python2
+    build_python3
+    cleanup
+}
