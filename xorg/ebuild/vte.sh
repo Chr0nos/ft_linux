4dad0c17204cbb602cffff64e7ab26e6d7ed5138
Chr0nos <snicolet@student.42.fr>
2018-04-08 16:38:24 +0000

    updated vte

--- xorg/ebuild/vte.sh
+++ xorg/ebuild/vte.sh
@@ -1,5 +1,5 @@
 build() {
-    VERSION="0.50.2"
+    VERSION="0.52.0"
     EXT="xz"
     URL="http://ftp.gnome.org/pub/gnome/sources/$PKG/0.50/$PKG-$VERSION.tar.$EXT"
     CFG="--sysconfdir=/etc --disable-static --enable-introspection"
