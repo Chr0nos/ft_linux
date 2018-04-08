55819c61a78b357c9e7d6a55720725fbec069e93
Chr0nos <snicolet@student.42.fr>
2018-04-08 16:39:08 +0000

    fix url

--- xorg/ebuild/vte.sh
+++ xorg/ebuild/vte.sh
@@ -1,7 +1,7 @@
 build() {
     VERSION="0.52.0"
     EXT="xz"
-    URL="http://ftp.gnome.org/pub/gnome/sources/$PKG/0.50/$PKG-$VERSION.tar.$EXT"
+    URL="http://ftp.gnome.org/pub/gnome/sources/$PKG/0.52/$PKG-$VERSION.tar.$EXT"
     CFG="--sysconfdir=/etc --disable-static --enable-introspection"
     build_generic
 }
