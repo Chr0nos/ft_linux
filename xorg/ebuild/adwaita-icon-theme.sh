a7fcf54e6f0b8147f32361012bb38ab269e5bc2f
Chr0nos <snicolet@student.42.fr>
2018-04-08 14:01:07 +0000

    fix url

--- xorg/ebuild/adwaita-icon-theme.sh
+++ xorg/ebuild/adwaita-icon-theme.sh
@@ -1,6 +1,6 @@
 build() {
-	VERSION="3.28"
-	URL="https://ftp.gnome.org/pub/GNOME/sources/adwaita-icon-theme/3.28/$PKG-$VERSION.tar.xz"
+	VERSION="3.28.0"
+	URL="https://ftp.gnome.org/pub/GNOME/sources/$PKG/3.28/$PKG-$VERSION.tar.xz"
 	prepair $PKG $VERSION xz $URL
 	./configure --prefix=/usr && compile $PKG-$VERSION && make install && cleanup
 }
