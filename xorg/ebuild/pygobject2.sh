d723655ef0bdbeb061f9af1219a2a6201df34a90
Chr0nos <snicolet@student.42.fr>
2018-04-08 19:53:34 +0000

    opti

--- xorg/ebuild/pygobject2.sh
+++ xorg/ebuild/pygobject2.sh
@@ -2,7 +2,8 @@ build() {
     VERSION="2.28.7"
     EXT="xz"
     URL="http://ftp.gnome.org/pub/gnome/sources/pygobject/2.28/pygobject-$VERSION.tar.$EXT"
+    CFG="--disable-introspection"
     pull $PKG $EXT $VERSION $URL
     unpack pygobject-$VERSION $EXT pygobject2-$VERSION.tar.$EXT
-    make install && cleanup
+    build_generic
 }
