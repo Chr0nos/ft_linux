ec34657397dbb2973faa2e11e0387b9a164c2aa7
Chr0nos <snicolet@student.42.fr>
2018-04-08 19:51:58 +0000

    fix

--- xorg/ebuild/pygobject2.sh
+++ xorg/ebuild/pygobject2.sh
@@ -2,6 +2,7 @@ build() {
     VERSION="2.28.7"
     EXT="xz"
     URL="http://ftp.gnome.org/pub/gnome/sources/pygobject/2.28/pygobject-$VERSION.tar.$EXT"
-    prepair $PKG $VERSION $EXT $URL
+    pull $PKG $EXT $VERSION $URL
+    unpack $PKG $EXT pygobject-$VERSION
     make install && cleanup
 }
