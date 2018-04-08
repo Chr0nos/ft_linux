6830c01b6def9c47b2e451f22c0e229c4bdd03f6
Chr0nos <snicolet@student.42.fr>
2018-04-08 19:42:14 +0000

    fix

--- xorg/ebuild/pycairo.sh
+++ xorg/ebuild/pycairo.sh
@@ -10,5 +10,6 @@ build() {
     VERSION="1.16.3"
     EXT="gz"
     URL="https://github.com/pygobject/pycairo/releases/download/v1.16.3/pycairo-$VERSION.tar.$EXT"
+    prepair $PKG $VERSION $EXT $URL
     build_python2 && build_python3 && cleanup
 }
