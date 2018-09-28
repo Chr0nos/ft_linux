712e8293519dbb0f621c1a7d808ea2f043c36bce
Sebastien NICOLET <snicolet@student.42.fre>
2018-09-28 12:09:14 +0000

    fix usage memoire de gdb...

--- xorg/ebuild/gdb.sh
+++ xorg/ebuild/gdb.sh
@@ -5,5 +5,5 @@ build() {
 	URL="https://ftp.gnu.org/gnu/$PKG/$PKG-$VERSION.tar.xz"
 	lprepair
 	./configure --prefix=$XORG_PREFIX --with-system-readline --without-guile &&
-		compile && make -C gdb install && cleanup
+		compile $PKG-$VERSION 2 && make -C gdb install && cleanup
 }
