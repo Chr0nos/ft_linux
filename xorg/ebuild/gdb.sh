b8a242e00c483ff3d2959861449bd2ae06cb0b51
Chr0nos <snicolet@student.42.fr>
2018-09-28 13:10:04 +0200

    typo

--- xorg/ebuild/gdb.sh
+++ xorg/ebuild/gdb.sh
@@ -1,6 +1,7 @@
 build() {
 	PKG="gdb"
 	VERSION="8.1"
+	EXT="xz"
 	URL="https://ftp.gnu.org/gnu/$PKG/$PKG-$VERSION.tar.xz"
 	lprepair
 	./configure --prefix=$XORG_PREFIX --with-system-readline --without-guile &&
